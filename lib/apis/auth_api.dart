0b4339869d025a260c8204e13ba8765c37c8dbfb
anirudh <anirudhkailkhura@gmail.com>
2023-08-18 17:26:52 +0530

    commit

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,91 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/provider.dart';
+import '../core/core.dart';
+
+//when you sign up, to get your accout -> Account
+//what to access user related data -> model.User previo us model.accoout
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthAPI(account: account);
+});
+
+abstract class IAuthAPI {
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  });
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  });
+
+  Future<model.User?> currentUserAccount();
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  //current user
+  @override
+  Future<model.User?> currentUserAccount() async {
+    try {
+      return await _account.get();
+    } on AppwriteException {
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  //siging up the user detail
+  @override
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+
+  //loggin the user detail
+  @override
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final session = await _account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      return right(session);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  //final ipAddress = InternetAddress('192.168.1.5');
+  static const String projectId = '64df06dbd8f8dd3c4b36';
+  static const String databaseId = '64df078a4abfc151c0c1';
+  static const String endPoint = 'http://192.168.1.5:80/v1';
+}
